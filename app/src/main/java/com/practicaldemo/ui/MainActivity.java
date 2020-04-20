eb4964f8d7e74489377f8f24c1cf35d73b29e02d
Rakshit <janirakshi9@gmail.com>
2020-04-20 14:56:15 +0530

    Structure

--- app/src/main/java/com/practicaldemo/ui/MainActivity.java
+++ app/src/main/java/com/practicaldemo/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.practicaldemo;
+package com.practicaldemo.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.practicaldemo.R;
 
 public class MainActivity extends AppCompatActivity {
 
